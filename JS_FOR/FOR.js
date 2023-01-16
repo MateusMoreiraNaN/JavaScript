9377ca4f7fc14858fd360e62e5e512a314476f2e
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-01-16 15:55:08 -0300

    for

---
+++ JS_FOR/FOR.js
@@ -0,0 +1,11 @@
+let contador = 1
+
+for(let i = 1; i <= 100; i++){
+    console.log(`i = ${i}`);
+}
+
+const notas = [6.7, 7.4, 9.8, 8.1, 7.1]
+
+for(i = 0; i < notas.length; i++){
+    console.log(notas[i]);
+}
