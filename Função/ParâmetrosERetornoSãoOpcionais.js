3a5ce7dcb1de1ba84ff764b5f2d71c6c65eb80f8
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-01-18 13:37:57 -0300

    area

---
+++ Função/ParâmetrosERetornoSãoOpcionais.js
@@ -0,0 +1,15 @@
+function area(largura, altura){
+    const area = largura * altura
+
+    if(area > 20){
+        console.log(`Valor acima do permitido: ${area}m2.`);
+    }else {
+        return area
+    }
+}
+
+console.log(area(2, 1));
+console.log(area(2, 2));
+console.log(area(2, 5));
+console.log(area(2, 3, 5, 7));
+console.log(area(45, 78));
