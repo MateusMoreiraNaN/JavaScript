5d12ee9594aa5be255991ae5440dfff0f2e75305
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-01-20 11:52:18 -0300

    padrao

--- Função/paramPadrao.js
+++ Função/paramPadrao.js
@@ -5,4 +5,20 @@ function soma1(a,b,c){
     return a + b + c
 }
 
-console.log(soma1());
+console.log(soma1(), soma1(3), soma1(5, 4, 5));
+
+function soma2(a, b, c) {
+    a = a !== undefined ? a : 1
+    b = 1 in arguments ? b : 1
+    c = isNaN(c) ? 1 : c
+    return a + b + c
+}
+
+console.log(soma2(), soma2(3), soma2(1,2,3));
+
+function soma3(a = 1, b = 1, c = 1){
+    return a + b + c
+}
+
+console.log(soma3(), soma3(3), soma3(1,2,3));
+
