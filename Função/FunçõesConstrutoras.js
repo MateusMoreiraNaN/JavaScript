b7f2b40d4cc0d82c86de8ce6ffa0aaa5153e0456
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-01-21 15:15:55 -0300

    Funções Construtoras

---
+++ Função/FunçõesConstrutoras.js
@@ -0,0 +1,29 @@
+function Carro(velocMaxima = 200, delta = 5){
+    let velocAtual = 0
+
+    this.acelerar = () => {
+        if(velocAtual + delta <= velocMaxima){
+            velocAtual += delta
+        }else{
+            velocAtual = velocMaxima
+        }
+    }
+
+    this.getVelocAtual = () => {
+        return velocAtual
+    }
+}
+
+const uno = new Carro
+uno.acelerar()
+console.log(uno.getVelocAtual())
+
+const ferrari = new Carro(350, 20)
+ferrari.acelerar()
+ferrari.acelerar()
+ferrari.acelerar()
+ferrari.acelerar()
+ferrari.acelerar()
+ferrari.acelerar()
+
+console.log(ferrari.getVelocAtual());
