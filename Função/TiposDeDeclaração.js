591e0c58d9b19022fc5814f6e1fcd87397eeac0b
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-01-23 09:49:18 -0300

    tipos

---
+++ Função/TiposDeDeclaração.js
@@ -0,0 +1,13 @@
+//console.log(soma(3, 4));
+
+function soma(x, y){
+    return x + y
+}
+
+const sub = function(x, y){
+    return x - y
+}
+
+const mult = function mult(x, y){
+    return x * y
+}
