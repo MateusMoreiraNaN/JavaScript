6be584b2856135197152eb9a465775a529e1d8ca
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-01-24 14:25:21 -0300

    OO

---
+++ Objeto/OO.js
@@ -0,0 +1,14 @@
+processamento(valor1, valor2, valor3)
+
+// OO
+
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        console.log('...');
+    }
+}
+
+objeto.processamento
