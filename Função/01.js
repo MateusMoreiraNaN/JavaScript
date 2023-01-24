7e9db0944d92d727503a0bf023253edba7b34078
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-01-24 13:30:35 -0300

    01

---
+++ Função/01.js
@@ -0,0 +1,15 @@
+function calc(n1, n2){
+    let soma = n1 + n2
+    let sub = n1 - n2
+    let div = n1 * n2 
+    let mult = n1  / n2
+
+    console.log(soma);
+    console.log(sub);
+    console.log(div);
+    console.log(mult);
+}
+
+calc(10, 5)
+
+
